7ccd255e0e58503baee48563cba18e6803caa1d0
mattrossman <rossman@gatech.edu>
2022-04-13 11:35:37 -0400

    Remape expression ratings to 1-5 integers

--- src/cleanup.js
+++ src/cleanup.js
@@ -1,5 +1,5 @@
-import csv from "./data/qualtrics_export.csv"
-const capture_data_files = import.meta.globEager("./data/face_data/*.json")
+import csv from "/data/qualtrics_export.csv"
+const capture_data_files = import.meta.globEager("/data/face_data/*.json")
 
 const capture_data = Object.fromEntries(
   Object.entries(capture_data_files).map(([filename, module]) => {
@@ -9,8 +9,20 @@ const capture_data = Object.fromEntries(
   })
 )
 
-const responses = csv.slice(2).filter((row) => row["Q33"].length > 0 && row["Q33"] !== "301007")
+const rating_labels = ["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"]
 
-console.log(capture_data)
+function remapRatings(row) {
+  for (const key in row) {
+    if (key.startsWith("Expression")) {
+      row[key] = rating_labels.indexOf(row[key]) + 1
+    }
+  }
+}
 
-Object.assign(window, { csv, responses, capture_data_files, capture_data })
+let responses = csv.slice(2).filter((row) => row["Q33"].length > 0 && row["Q33"] !== "301007")
+responses.map((row) => remapRatings(row))
+const responesObj = Object.fromEntries(responses.map((row) => [row["Q33"], row]))
+
+console.log(responesObj)
+
+Object.assign(window, { csv, responses, responesObj, capture_data_files, capture_data })
